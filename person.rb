50c71ff530830de8ef865ff8e68fe79f6d6a9cf4
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-04 18:26:36 +0300

    Add class decorator

--- person.rb
+++ person.rb
@@ -1,5 +1,7 @@
-class Person
-  def initalalize(age, parent_permission: true, name: 'Unknown')
+require './nameable.rb'
+
+class Person < Nameable
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -9,13 +11,20 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
+  def can_use_servicies?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
-
-  def can_use_servicies?
-    of_age? || @parent_permission
-  end
 end
+
+person = Person.new(18, 'maximilianus')
+puts person.correct_name
